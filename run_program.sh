71e37a3287a32438ea887ea8e87eb30635743e12
Pedro Henrique Kopper <phckopper@inf.ufrgs.br>
2023-01-30 11:58:20 -0300

    Add support for building nanopb along our application

--- run_program.sh
+++ run_program.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo "Generating protocol buffers..."
+
+python3 ./nanopb/generator/nanopb_generator.py messages.proto
+if [ $? -eq 0 ]; then
+    echo "Protobufs generated successfully"
+else
+    echo "Protobuf generation failed."
+    echo "Check out if required software is installed using"
+    echo "sudo apt install python3 python3-pip"
+    echo "pip3 install protobuf grpcio-tools"
+    exit 1
+fi
+
 echo "Compiling..."
-gcc -o meuPrograma main.c discovery_service.c management_service.c -lpthread
+gcc -Wall -o meuPrograma *.c nanopb/*.c -lpthread -I nanopb
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful!"
