3f7f1191e2f9e2a7cd638f95ead585e52136958f
brendaschussler <brendaschussler@gmail.com>
2023-02-08 22:34:58 -0300

    test find in listen_discovery

--- discovery_service.c
+++ discovery_service.c
@@ -110,7 +110,7 @@ void *listen_discovery(void *args)
     {
         if (!message_shown)
         {
-            
+
             printf("Aguardando um novo participante entrar...\n");
             printf("\n");
             message_shown = 1;
@@ -131,194 +131,197 @@ void *listen_discovery(void *args)
             char hostname[256], ip_address[16];
             getnameinfo((struct sockaddr *)&cli_addr, clilen, hostname, sizeof(hostname), NULL, 0, 0);
             inet_ntop(AF_INET, &(cli_addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
-            add_participant(hostname, ip_address, msg.mac_address, msg.status, PARTICIPANT_TIMEOUT);
-            char mac_adress_manager[18];
-            get_mac_address(mac_adress_manager);
-
-            send_type_msg(&serv_addr, manlen, mac_adress_manager, ip_address, CONFIRMED_TYPE);
-        }
-    }
-    close(sockfd);
-    pthread_exit(NULL);
-}
-
-void participant_start()
-{
-    // Listen for manager broadcast
-    struct hostent *host_entry;
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT);
-    socklen_t manager_addrlen = sizeof(addr);
-
-    char hostname[256], ip_address[16], mac_address[18];
-
-    // Obtém o hostname
-    // gethostname(hostname, 256);
-
-    getnameinfo((struct sockaddr *)&addr, manager_addrlen, hostname, sizeof(hostname), NULL, 0, 0);
-    inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
-
-    // Obtém informações sobre o host
-    // host_entry = gethostbyname(hostname);
-
-    // Obtém o endereço IP
-    // inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
-
-    // getnameinfo((struct sockaddr *)&addr, manager_addrlen, hostname, sizeof(hostname), NULL, 0, 0);
-    // inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
-    get_mac_address(mac_address);
-    // printf("o ip aqui eh\n: %s", ip_address);
-    add_participant_noprint(hostname, ip_address, mac_address, 1, PARTICIPANT_TIMEOUT);
-    send_discovery_msg(sockfd, &addr, manager_addrlen, mac_address);
-
-    // send_discovery_msg(sockfd, &addr, manager_addrlen);
-    close(sockfd);
-}
-
-void get_mac_address(char *mac_address)
-{
-    struct ifreq ifr;
-    struct ifconf ifc;
-    char buf[1024];
-    int success = 0;
-
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (sock == -1)
-    {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    ifc.ifc_len = sizeof(buf);
-    ifc.ifc_buf = buf;
-    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1)
-    {
-        perror("ioctl");
-        exit(EXIT_FAILURE);
-    }
-
-    struct ifreq *it = ifc.ifc_req;
-    const struct ifreq *const end = it + (ifc.ifc_len / sizeof(struct ifreq));
-
-    for (; it != end; ++it)
-    {
-        strcpy(ifr.ifr_name, it->ifr_name);
-        if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0)
-        {
-            if (!(ifr.ifr_flags & IFF_LOOPBACK))
+            int index = find_participant(msg.mac_address);
+            if (index == -1) //não achou na lista 
             {
-                if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0)
-                {
-                    success = 1;
-                    break;
-                }
+                add_participant(hostname, ip_address, msg.mac_address, msg.status, PARTICIPANT_TIMEOUT);
+            }
+                char mac_adress_manager[18];
+                get_mac_address(mac_adress_manager);
+
+                send_type_msg(&serv_addr, manlen, mac_adress_manager, ip_address, CONFIRMED_TYPE);
             }
         }
-        else
+        close(sockfd);
+        pthread_exit(NULL);
+    }
+
+    void participant_start()
+    {
+        // Listen for manager broadcast
+        struct hostent *host_entry;
+        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(PORT);
+        socklen_t manager_addrlen = sizeof(addr);
+
+        char hostname[256], ip_address[16], mac_address[18];
+
+        // Obtém o hostname
+        // gethostname(hostname, 256);
+
+        getnameinfo((struct sockaddr *)&addr, manager_addrlen, hostname, sizeof(hostname), NULL, 0, 0);
+        inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
+
+        // Obtém informações sobre o host
+        // host_entry = gethostbyname(hostname);
+
+        // Obtém o endereço IP
+        // inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
+
+        // getnameinfo((struct sockaddr *)&addr, manager_addrlen, hostname, sizeof(hostname), NULL, 0, 0);
+        // inet_ntop(AF_INET, &(addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
+        get_mac_address(mac_address);
+        // printf("o ip aqui eh\n: %s", ip_address);
+        add_participant_noprint(hostname, ip_address, mac_address, 1, PARTICIPANT_TIMEOUT);
+        send_discovery_msg(sockfd, &addr, manager_addrlen, mac_address);
+
+        // send_discovery_msg(sockfd, &addr, manager_addrlen);
+        close(sockfd);
+    }
+
+    void get_mac_address(char *mac_address)
+    {
+        struct ifreq ifr;
+        struct ifconf ifc;
+        char buf[1024];
+        int success = 0;
+
+        int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+        if (sock == -1)
+        {
+            perror("socket");
+            exit(EXIT_FAILURE);
+        }
+
+        ifc.ifc_len = sizeof(buf);
+        ifc.ifc_buf = buf;
+        if (ioctl(sock, SIOCGIFCONF, &ifc) == -1)
         {
             perror("ioctl");
             exit(EXIT_FAILURE);
         }
-    }
-    if (success)
-    {
-        char formatted_mac_address[18];
-        memcpy(formatted_mac_address, ifr.ifr_hwaddr.sa_data, 6);
-        sprintf(formatted_mac_address, "%02x:%02x:%02x:%02x:%02x:%02x",
-                (unsigned char)formatted_mac_address[0],
-                (unsigned char)formatted_mac_address[1],
-                (unsigned char)formatted_mac_address[2],
-                (unsigned char)formatted_mac_address[3],
-                (unsigned char)formatted_mac_address[4],
-                (unsigned char)formatted_mac_address[5]);
-        strcpy(mac_address, formatted_mac_address);
-    }
-}
 
-void *listen_Confirmed(void *args)
-{
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    socklen_t manlen = sizeof(serv_addr);
-    packet msg;
-    participant new_participant1;
+        struct ifreq *it = ifc.ifc_req;
+        const struct ifreq *const end = it + (ifc.ifc_len / sizeof(struct ifreq));
 
-    // Create socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-    {
-        printf("Error opening socket");
-        pthread_exit(NULL);
-    }
-
-    // Bind socket to address and port
-    memset((char *)&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(RESPONSE_PORT);
-    bzero(&(serv_addr.sin_zero), 8);
-
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("Error on binding");
-        pthread_exit(NULL);
-    }
-    struct sockaddr_in cli_addr;
-    socklen_t clilen = sizeof(cli_addr);
-    char mac_address[18];
-    while (1)
-    {
-        
-        // Receive message
-        int n = recvfrom(sockfd, &msg, sizeof(msg), 0, (struct sockaddr *)&cli_addr, &clilen);
-        if (n < 0)
+        for (; it != end; ++it)
         {
-            printf("Error on recvfrom");
+            strcpy(ifr.ifr_name, it->ifr_name);
+            if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0)
+            {
+                if (!(ifr.ifr_flags & IFF_LOOPBACK))
+                {
+                    if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0)
+                    {
+                        success = 1;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                perror("ioctl");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (success)
+        {
+            char formatted_mac_address[18];
+            memcpy(formatted_mac_address, ifr.ifr_hwaddr.sa_data, 6);
+            sprintf(formatted_mac_address, "%02x:%02x:%02x:%02x:%02x:%02x",
+                    (unsigned char)formatted_mac_address[0],
+                    (unsigned char)formatted_mac_address[1],
+                    (unsigned char)formatted_mac_address[2],
+                    (unsigned char)formatted_mac_address[3],
+                    (unsigned char)formatted_mac_address[4],
+                    (unsigned char)formatted_mac_address[5]);
+            strcpy(mac_address, formatted_mac_address);
+        }
+    }
+
+    void *listen_Confirmed(void *args)
+    {
+        int sockfd;
+        struct sockaddr_in serv_addr;
+        socklen_t manlen = sizeof(serv_addr);
+        packet msg;
+        participant new_participant1;
+
+        // Create socket
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+        {
+            printf("Error opening socket");
             pthread_exit(NULL);
         }
-        if (msg.type == CONFIRMED_TYPE)
+
+        // Bind socket to address and port
+        memset((char *)&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serv_addr.sin_port = htons(RESPONSE_PORT);
+        bzero(&(serv_addr.sin_zero), 8);
+
+        if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         {
-            char hostname[256], ip_address[16];
-            getnameinfo((struct sockaddr *)&cli_addr, clilen, hostname, sizeof(hostname), NULL, 0, 0);
-            inet_ntop(AF_INET, &(cli_addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
-            /*int att_dados = add_participant(hostname, ip_address, msg.mac_address, msg.status);
-            if (att_dados == 0)
-            {
-                // printf("----------------------------------\n");
-                printf("Esse é o endereço de seu Manager!\n");
-                printf("----------------------------------\n");
-            }*/
-            // char mac_address[18];
-            if (strcmp(mac_address, msg.mac_address) != 0)
-            {
-                /*printf("------------------------------------------------\n");
-                printf("       Esse é o endereço de seu Manager!\n");
-                printf("------------------------------------------------\n");
-                printf("Hostname: %s\n", hostname);
-                printf("IP address: %s\n", ip_address);
-                printf("MAC address: %s\n", msg.mac_address);
-                strcpy(mac_address, msg.mac_address);
-                if (msg.status == 1)
-                {
-                    printf("Status: awaken\n");
-                }
-                else
-                {
-                    printf("Status: asleep\n");
-                }
-                printf("------------------------------------------------\n");
-                printf("\n");*/
-                strcpy(manager.hostname, hostname);
-                strcpy(manager.ip_address, ip_address);
-                strcpy(manager.mac_address, msg.mac_address);
-                manager.status = 1;
-                sem_post(&sem_update_interface);
-            }
-            
+            printf("Error on binding");
+            pthread_exit(NULL);
         }
+        struct sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+        char mac_address[18];
+        while (1)
+        {
+
+            // Receive message
+            int n = recvfrom(sockfd, &msg, sizeof(msg), 0, (struct sockaddr *)&cli_addr, &clilen);
+            if (n < 0)
+            {
+                printf("Error on recvfrom");
+                pthread_exit(NULL);
+            }
+            if (msg.type == CONFIRMED_TYPE)
+            {
+                char hostname[256], ip_address[16];
+                getnameinfo((struct sockaddr *)&cli_addr, clilen, hostname, sizeof(hostname), NULL, 0, 0);
+                inet_ntop(AF_INET, &(cli_addr.sin_addr.s_addr), ip_address, INET_ADDRSTRLEN);
+                /*int att_dados = add_participant(hostname, ip_address, msg.mac_address, msg.status);
+                if (att_dados == 0)
+                {
+                    // printf("----------------------------------\n");
+                    printf("Esse é o endereço de seu Manager!\n");
+                    printf("----------------------------------\n");
+                }*/
+                // char mac_address[18];
+                if (strcmp(mac_address, msg.mac_address) != 0)
+                {
+                    /*printf("------------------------------------------------\n");
+                    printf("       Esse é o endereço de seu Manager!\n");
+                    printf("------------------------------------------------\n");
+                    printf("Hostname: %s\n", hostname);
+                    printf("IP address: %s\n", ip_address);
+                    printf("MAC address: %s\n", msg.mac_address);
+                    strcpy(mac_address, msg.mac_address);
+                    if (msg.status == 1)
+                    {
+                        printf("Status: awaken\n");
+                    }
+                    else
+                    {
+                        printf("Status: asleep\n");
+                    }
+                    printf("------------------------------------------------\n");
+                    printf("\n");*/
+                    strcpy(manager.hostname, hostname);
+                    strcpy(manager.ip_address, ip_address);
+                    strcpy(manager.mac_address, msg.mac_address);
+                    manager.status = 1;
+                    sem_post(&sem_update_interface);
+                }
+            }
+        }
+        close(sockfd);
+        pthread_exit(NULL);
     }
-    close(sockfd);
-    pthread_exit(NULL);
-}
